4751679441f4c5b985eb0ca7343bcd137348c91e
AhmedU801 <ahmeduddinict@gmail.com>
2019-11-10 00:26:13 -0500

    added score

--- tetris.py
+++ tetris.py
@@ -195,7 +195,7 @@ def draw_text_middle(text, size, color, surface):
     label = font.render(text, 1, color)
 
     surface.blit(label, (
-    top_left_x + play_width / 2 - (label.get_width() / 2), top_left_y + play_height / 2 - label.get_height() / 2))
+        top_left_x + play_width / 2 - (label.get_width() / 2), top_left_y + play_height / 2 - label.get_height() / 2))
 
 
 def draw_grid(surface, row, col):
@@ -231,6 +231,8 @@ def clear_rows(grid, locked):
                 newKey = (x, y + inc)
                 locked[newKey] = locked.pop(key)
 
+    return inc
+
 
 def draw_next_shape(shape, surface):
     font = pygame.font.SysFont('comicsans', 30)
@@ -249,7 +251,7 @@ def draw_next_shape(shape, surface):
     surface.blit(label, (sx + 10, sy - 30))
 
 
-def draw_window(surface):
+def draw_window(surface, score=0, last_score=0):
     surface.fill((0, 0, 0))
     # Tetris Title
     font = pygame.font.SysFont('comicsans', 60)
@@ -257,19 +259,50 @@ def draw_window(surface):
 
     surface.blit(label, (top_left_x + play_width / 2 - (label.get_width() / 2), 30))
 
+    # current score
+    font = pygame.font.SysFont('comicsans', 30)
+    label = font.render('Score: {}'.format(score), 1, (255, 255, 255))
+
+    sx = top_left_x + play_width + 50
+    sy = top_left_y + play_height / 2 - 100
+
+    surface.blit(label, (sx + 20, sy + 160))
+
+    # high score
+    label = font.render('High Score: {}'.format(last_score), 1, (255, 255, 255))
+
+    sx = top_left_x - 200
+    sy = top_left_y + 200
+
+    surface.blit(label, (sx + 20, sy + 160))
+
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             pygame.draw.rect(surface, grid[i][j], (top_left_x + j * 30, top_left_y + i * 30, 30, 30), 0)
 
-    # draw grid and border
     draw_grid(surface, 20, 10)
     pygame.draw.rect(surface, (255, 0, 0), (top_left_x, top_left_y, play_width, play_height), 5)
-    # pygame.display.update()
+
+
+def update_score(nscore, score):
+    with open('high_score.txt', 'w') as f:
+        if int(score) <= nscore:
+            f.write(str(nscore))
+
+
+def max_score():
+    with open('high_score.txt', "r") as f:
+        lines = f.readlines()
+        score = lines[0].strip()
+
+    return score
 
 
 def main():
     global grid
 
+    score = 0
+    last_score = max_score()
     locked_positions = {}  # (x,y):(255,0,0)
     grid = create_grid(locked_positions)
 
@@ -323,12 +356,6 @@ def main():
                     if not valid_space(current_piece, grid):
                         current_piece.y -= 1
 
-                '''if event.key == pygame.K_SPACE:
-                   while valid_space(current_piece, grid):
-                       current_piece.y += 1
-                   current_piece.y -= 1
-                   print(convert_shape_format(current_piece))'''  # todo fix
-
         shape_pos = convert_shape_format(current_piece)
 
         # add piece to the grid for drawing
@@ -347,15 +374,19 @@ def main():
             change_piece = False
 
             # call four times to check for multiple clear rows
-            clear_rows(grid, locked_positions)
+            score += clear_rows(grid, locked_positions) * 10
 
-        draw_window(win)
+        draw_window(win, score, last_score)
         draw_next_shape(next_piece, win)
         pygame.display.update()
 
         # Check if user lost
         if check_lost(locked_positions):
+            draw_text_middle("You lost!", 80, (255, 255, 255), win)
+            pygame.display.update()
+            pygame.time.delay(1500)
             run = False
+            update_score(score, last_score)
 
     draw_text_middle("You Lost", 40, (255, 255, 255), win)
     pygame.display.update()
